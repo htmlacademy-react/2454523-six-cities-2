e166862fd22ee1dab11da991468000391469c2f0
Maria-Samoshkina <150549726+Maria-Samoshkina@users.noreply.github.com>
2025-03-24 21:00:20 +0200

    реализует сортировку отзывов: от новых к старым

--- src/pages/offer-screen/reviews-list.tsx
+++ src/pages/offer-screen/reviews-list.tsx
@@ -1,13 +1,16 @@
 import { useAppSelector } from '../../hooks';
 import ReviewForm from './review-form';
 import ReviewItem from './review-item';
-import { AuthorizationStatus} from '../../const';
+import { AuthorizationStatus, MAX_COMMENT_LENGTH} from '../../const';
 
 
 function ReviewsList(): JSX.Element {
 
   const reviews = useAppSelector((state)=> state.reviews);
   const autorizationStatus = useAppSelector((state)=> state.authorizationStatus);
+  const reviewsForRendering = [...reviews]
+    .sort((a,b)=> new Date(b.date).getTime() - new Date(a.date).getTime())
+    .slice(0, MAX_COMMENT_LENGTH);
 
   return (
     <section className="offer__reviews reviews">
@@ -16,7 +19,7 @@ function ReviewsList(): JSX.Element {
         <span className="reviews__amount">{reviews.length}</span>
       </h2>
       <ul className="reviews__list">
-        {reviews.map((review) => (
+        {reviewsForRendering.map((review) => (
           <ReviewItem key={review.id} review={review} />
         ))}
       </ul>
