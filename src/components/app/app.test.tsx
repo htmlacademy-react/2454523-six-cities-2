b98f755d731bab29269cb69b59d57dc9b74a02c0
Maria-Samoshkina <150549726+Maria-Samoshkina@users.noreply.github.com>
2025-04-26 14:24:49 +0300

    добавит тесты для проверки маршрутизации

---
+++ src/components/app/app.test.tsx
@@ -0,0 +1,177 @@
+import { render, screen} from '@testing-library/react';
+import { MemoryHistory, createMemoryHistory } from 'history';
+import { AppRoute, AuthorizationStatus, SortType } from '../../const';
+import App from './app';
+import { withStore, withHistory } from '../../utilsMocks/mock-component';
+import { makeFakeComment, makeFakeDetailedOffer, makeFakeOffer } from '../../utilsMocks/mocks';
+
+
+describe('Application Routing', () => {
+  let mockHistory: MemoryHistory;
+
+  beforeEach(() => {
+    mockHistory = createMemoryHistory();
+  });
+
+  it('should render MainScreen when user navigate to "/"', () => {
+    const withHistoryComponent = withHistory(<App />, mockHistory);
+
+    const offer = makeFakeOffer();
+
+    const { withStoreComponent } = withStore(
+      withHistoryComponent,
+      {
+        OFFERS: {
+          isOffersDataLoading: false,
+          offers: [offer],
+          isOffersFetchingError: false,
+        },
+        FILTERS: {
+          city: offer.city.name,
+          sortType: SortType.Popular,
+        },
+        USER: {
+          authorizationStatus: AuthorizationStatus.Auth,
+          userEmail: 'test'
+        },
+        FAVORITES: {
+          isFavoritesFetchingError: false,
+          isFavoritesLoading: true,
+          favorites: []
+        }
+      }
+    );
+
+
+    render(withStoreComponent);
+
+    expect(screen.getByText(new RegExp(offer.title, 'i'))).toBeInTheDocument();
+    expect(screen.getByText(/Cities/i)).toBeInTheDocument();
+    expect(screen.getByText(new RegExp(`${offer.price}`))).toBeInTheDocument();
+    expect(screen.getByText(/Paris/i)).toBeInTheDocument();
+    expect(screen.getByText(new RegExp(`${offer.type}`))).toBeInTheDocument();
+
+  });
+
+
+  it('should render FavoriteScreen when user navigate to "/favorites"', () => {
+    const withHistoryComponent = withHistory(<App />, mockHistory);
+
+    const favoriteOffer = makeFakeOffer();
+
+    const { withStoreComponent } = withStore(
+      withHistoryComponent,
+      {
+        OFFERS: {
+          isOffersDataLoading: false,
+          offers: [favoriteOffer],
+          isOffersFetchingError: false,
+        },
+        USER: {
+          authorizationStatus: AuthorizationStatus.Auth,
+          userEmail: 'test'
+        },
+        FAVORITES: {
+          isFavoritesFetchingError: false,
+          isFavoritesLoading: false,
+          favorites: [favoriteOffer]
+        }
+      }
+    );
+    mockHistory.push(AppRoute.Favorites);
+
+    render(withStoreComponent);
+
+    expect(screen.getByText(new RegExp(favoriteOffer.title, 'i'))).toBeInTheDocument();
+    expect(screen.getByText(new RegExp(`${favoriteOffer.price}`))).toBeInTheDocument();
+    expect(screen.getByText(new RegExp(`${favoriteOffer.type}`))).toBeInTheDocument();
+    expect(screen.getByText(/Saved listing/i)).toBeInTheDocument();
+  });
+
+
+  it('should render LoginScreen when user navigate to "/login"', () => {
+    const withHistoryComponent = withHistory(<App />, mockHistory);
+
+    const offer = makeFakeOffer();
+
+    const { withStoreComponent } = withStore(
+      withHistoryComponent,
+      {
+        OFFERS: {
+          isOffersDataLoading: false,
+          offers: [offer],
+          isOffersFetchingError: false,
+        },
+        USER: {
+          authorizationStatus: AuthorizationStatus.NoAuth,
+          userEmail: ''
+        },
+      }
+    );
+
+    mockHistory.push(AppRoute.Login);
+
+    render(withStoreComponent);
+
+    expect(
+      screen.getByRole('heading', { level: 1, name: /Sign in/i })
+    ).toBeInTheDocument();
+    expect(
+      screen.getByRole('button', { name: /Sign in/i })
+    ).toBeInTheDocument();
+    expect(screen.getByPlaceholderText(/email/i)).toHaveAttribute('type', 'email');
+    expect(screen.getByPlaceholderText(/password/i)).toHaveAttribute('type', 'password');
+  });
+
+
+  it('should render OfferScreen when user navigate to "/offer/:offerId"', () => {
+    const withHistoryComponent = withHistory(<App />, mockHistory);
+
+    const offer = makeFakeOffer();
+    const detailedOffer = makeFakeDetailedOffer();
+    const comment = makeFakeComment();
+
+    const { withStoreComponent } = withStore(
+      withHistoryComponent,
+      {
+        OFFERS: {
+          isOffersDataLoading: false,
+          offers: [offer],
+          isOffersFetchingError: false,
+        },
+        USER: {
+          authorizationStatus: AuthorizationStatus.Auth,
+          userEmail: 'test'
+        },
+        FAVORITES: {
+          isFavoritesFetchingError: false,
+          isFavoritesLoading: true,
+          favorites: []
+        },
+        DETAILED_OFFER: {
+          neighboringOffers:[makeFakeOffer()],
+          detailedOffer: detailedOffer,
+          isDetailedOfferLoading: false,
+          isDetailedOfferFetchingError: false
+        },
+        REVIEWS: {
+          isSubmitting:false,
+          isSubmittingFailed: false,
+          isReviewsLoading: false,
+          reviews: [comment],
+          isReviewsFetchingError: false
+        }
+      }
+    );
+
+    mockHistory.push(`${AppRoute.Offer}/${offer.id}`);
+
+    render(withStoreComponent);
+
+    expect(screen.getByText(new RegExp(detailedOffer.title, 'i'))).toBeInTheDocument();
+    expect(screen.getByText(new RegExp(`${detailedOffer.price}`))).toBeInTheDocument();
+
+  });
+
+
+});
--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -12,8 +12,6 @@ import LoadingScreen from '../../pages/loading-screen/loading-screen';
 import { useEffect } from 'react';
 import { fetchOffersAction, checkAuthAction, fetchFavoritesOffersAction } from '../../store/api-actions';
 import { useAppDispatch } from '../../hooks';
-import HistoryRouter from '../history-route/history-route';
-import browserHistory from '../../browser-history';
 import FetchingError from '../error-message/fetching-error';
 import { getIsOffersDataLoading } from '../../store/offers/offers-selectors';
 import { getIsOffersFetchingError } from '../../store/offers/offers-selectors';
@@ -45,26 +43,24 @@ function App (): JSX.Element {
 
   return(
     <HelmetProvider>
-      <HistoryRouter history={browserHistory}>
-        <Routes>
-          <Route path={AppRoute.Main}>
-            <Route index element = {<MainScreen />}/>
-            <Route path = {AppRoute.Favorites} element = {
-              <PrivateRoute>
-                <FavoriteScreen/>
-              </PrivateRoute>
-            }
-            />
-            <Route path = {AppRoute.Login} element = {<LoginScreen/>}/>
-            <Route path={`${AppRoute.Offer}/:offerId`} element={
-              <OfferScreen/>
-            }
-            />
+      <Routes>
+        <Route path={AppRoute.Main}>
+          <Route index element = {<MainScreen />}/>
+          <Route path = {AppRoute.Favorites} element = {
+            <PrivateRoute>
+              <FavoriteScreen/>
+            </PrivateRoute>
+          }
+          />
+          <Route path = {AppRoute.Login} element = {<LoginScreen/>}/>
+          <Route path={`${AppRoute.Offer}/:offerId`} element={
+            <OfferScreen/>
+          }
+          />
 
-          </Route>
-          <Route path='*' element ={<NotFoundScreen/>}/>
-        </Routes>
-      </HistoryRouter>
+        </Route>
+        <Route path='*' element ={<NotFoundScreen/>}/>
+      </Routes>
     </HelmetProvider>
   );
 
