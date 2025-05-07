ce0cc260701a761b584a4997fe33b12d96190492
Maria-Samoshkina <150549726+Maria-Samoshkina@users.noreply.github.com>
2025-05-07 16:27:10 +0300

    доработает OfferScreen: . Клик по кнопке «Избранное» добавляет карточку в избранное. Если пользователь не авторизован, то выполняется перенаправление на страницу Login.

--- src/store/detailed-offer/detailed-offer-slice.ts
+++ src/store/detailed-offer/detailed-offer-slice.ts
@@ -1,7 +1,7 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { NameSpace } from '../../const';
 import { DetailedOfferState } from '../../types/state';
-import { fetchDetailedOfferAction } from '../api-actions';
+import { addToFavorites, fetchDetailedOfferAction, removeFromFavorites } from '../api-actions';
 import { MAX_COUNT_NEAR_OFFERS } from '../../const';
 
 
@@ -37,8 +37,17 @@ export const detailedOfferSlice = createSlice({
       .addCase(fetchDetailedOfferAction.rejected, (state)=> {
         state.isDetailedOfferFetchingError = true;
         state.isDetailedOfferLoading = false;
+      })
+      .addCase(addToFavorites.fulfilled, (state)=> {
+        if(state.detailedOffer){
+          state.detailedOffer.isFavorite = true;
+        }
+      })
+      .addCase(removeFromFavorites.fulfilled, (state)=> {
+        if(state.detailedOffer){
+          state.detailedOffer.isFavorite = false;
+        }
       });
-
   }
 });
 
